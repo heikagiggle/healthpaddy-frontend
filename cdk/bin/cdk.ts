75298f4c53a29ccbe6e38b792ea38a8df6e1c677
heikagiggle <okaforemmanuellaoluchi@gmail.com>
2024-11-11 19:25:51 +0100

    ran build

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
-import "dotenv/config"
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import {CdkStack} from "../lib/cdk-stack";
+// import "dotenv/config"
+// import 'source-map-support/register';
+// import * as cdk from 'aws-cdk-lib';
+// import {CdkStack} from "../lib/cdk-stack";
 
-const app = new cdk.App();
+// const app = new cdk.App();
 
-const appId = process.env['AWS_APP_ID'];
-if (!appId) throw new Error('App ID is missing');
+// const appId = process.env['AWS_APP_ID'];
+// if (!appId) throw new Error('App ID is missing');
 
-const props = {
-    stackName: `${appId}-hosting`,
-    env: {
-        account: process.env['CDK_DEFAULT_ACCOUNT'],
-        region: process.env['CDK_DEFAULT_REGION'],
-    },
-};
+// const props = {
+//     stackName: `${appId}-hosting`,
+//     env: {
+//         account: process.env['CDK_DEFAULT_ACCOUNT'],
+//         region: process.env['CDK_DEFAULT_REGION'],
+//     },
+// };
 
-new CdkStack(app, 'hosting', props);
+// new CdkStack(app, 'hosting', props);
